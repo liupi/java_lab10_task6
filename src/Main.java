e0d88dd36b95f9f870b69d400a1f9efd5ef4cab1
Liudmila <milalindley@gmail.com>
2025-12-04 16:43:50 +0100

    feat: get information from file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+       Scanner scan =  new Scanner(System.in);
+       System.out.println("Please enter three first digits of bank account: ");
+       int userInput  = scan.nextInt();
 
+       URL url = new URL("https://ewib.nbp.pl/plewibnra?dokNazwa=plewibnra.txt");
+       BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String str;
+        while ((str = rd.readLine()) != null) {
+            int bankNum = Integer.parseInt(str.substring(0, 6).trim());
+            if (bankNum == userInput){
+                String bankName = str.substring(6, 144).trim();
+                System.out.println("Bank name: " + bankName);
+                break;
+            }
+        }
+        rd.close();
     }
 }
