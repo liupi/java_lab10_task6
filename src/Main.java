4d1548eb2b54dd0e4d1277d187b38f7efc55970f
Liudmila <milalindley@gmail.com>
2025-12-04 16:45:14 +0100

    feat: get information from file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws Exception{
        Scanner scan =  new Scanner(System.in);
